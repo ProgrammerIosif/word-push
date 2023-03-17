e4556fe49514b5ee1ae5af8904b105bd0387e6a3
ProgrammerIosif <programmeriosif@protonmail.com>
2023-03-17 22:54:10 +0200

    added animated search box widget

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,70 @@
+import { useState,useEffect } from "react";
+
+const strings = [
+  'artificial-eloquence.com',
+  'pool-request.com',
+  'splash-cache.com',
+  'upload-down-code.com',
+  'browser-powder.com',
+  'front-end-hydration-station.com',
+  'full-stack-lipstick.com',
+]
+
+const Typewriter = () => {
+  const [activeText, setActiveText] = useState('');
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [index, setIndex] = useState(0);
+  const [speed, setSpeed] = useState(100);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const currentIndex = index % strings.length;
+      const currentString = strings[currentIndex];
+
+      if (!isDeleting) {
+        setActiveText((prev) => prev + currentString.charAt(prev.length));
+      } else {
+        setActiveText((prev) => prev.slice(0, -1));
+      }
+
+      if (activeText === currentString) {
+        setTimeout(() => {
+            setIsDeleting(true);
+          }, 1000);
+        setSpeed(50);
+      } else if (activeText === '' && isDeleting) {
+        setIsDeleting(false);
+        setIndex((prev) => prev + 1);
+        setSpeed(120);
+      }
+    }, speed);
+
+    return () => clearTimeout(timer);
+  }, [activeText, index, isDeleting, speed, strings]);
+
+  return (
+    <div className="flex justify-center pl-6 text-3xl">
+      <div className="">
+        {activeText}
+      </div>
+    </div>
+  );
+};
+
+function SearchBar() {
+  return (
+    <div className='relative bg-[#f1f8fe] h-28 w-full rounded-xl mt-16 shadow-lg p-5 flex gap-10'>
+      <div className="flex justify-around items-center h-full w-32 ml-6">
+        {[0,1,2].map(() =>
+          <div className="bg-[#d4dadf] h-5 w-5 rounded-full"></div>
+        )}
+      </div>
+      <div className="bg-white w-full h-full rounded-xl flex justify-start items-center px-6">
+        <img src="https://cdn-icons-png.flaticon.com/512/1828/1828471.png" alt="" className="h-6" />
+        <Typewriter/>
+      </div>
+    </div>
+  )
+}
+
+export default SearchBar;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,7 @@
 import ImageSlider from '../components/ImageSlider';
 import ImageGrid from '../components/ImageGrid';
 import ProductsMenu from '../components/ProductsMenu';
+import SearchBar from '../components/SearchBar';
 
 function Index() {
 
@@ -32,12 +33,25 @@ function Index() {
       <section className='mt-60 bg-white -mb-60'>
         <div className="pt-32 w-full max-w-6xl max-xl:px-5 mx-auto">
             <h1 className="font-serif text-7xl">Infinitely efortless editing</h1>
-            <p className="max-w-4xl text-lg font-normal mt-6">Magically drag, drop and draw on a huge collection of design patterns, features, and layouts with the intuitive, built-in block editor.</p>
+            <p className="max-w-xl text-lg font-normal mt-6">Magically drag, drop and draw on a huge collection of design patterns, features, and layouts with the intuitive, built-in block editor.</p>
             <button className="mt-10 bg-black text-white flex justify-center items-center w-40 h-14 rounded-md font-medium">Start creating</button>
             <video className="my-16" src="https://s1.wp.com/wp-content/themes/h4/assets/block-editor/2022-10@2x.mp4" autoPlay loop></video>
         </div>
       </section>
-      <section className=' bg-primary-button h-80'>
+      <section className=' bg-[#bbe0fa] pt-60 -mb-14'>
+        <div className="pt-32 w-full max-w-6xl max-xl:px-5 mx-auto">
+            <h1 className="font-serif text-7xl">Your home on the web</h1>
+            <p className="max-w-xl text-lg font-normal mt-6">Nothing sets your site apart faster than a unique address on the web. From <b>.com</b> to <b>.blog</b>, there’s a one-of-a-kind domain with your name on it. And your first year is free with any annual paid plan.</p>
+            <button className="mt-12 bg-black text-white flex justify-center items-center w-48 h-12 rounded-md font-medium">Find your domain</button>
+            <SearchBar/>
+        </div>
+      </section>
+      <section className='bg-[#e9eff5]'>
+        <div className="pt-60 w-full max-w-6xl max-xl:px-5 mx-auto">
+            <h1 className="font-serif text-7xl max-w-5xl">Basically, the best managed WordPress hosting on the planet.</h1>
+            <p className="max-w-3xl text-lg font-normal mt-6">Say hello to the next-level hosting performance made possible when you deeply integrate every piece of hardware and line of code to run WordPress at speed and scale.</p>
+            <button className="mt-12 bg-[#0675c4] text-white flex justify-center items-center w-48 h-12 rounded-md font-medium">Explore hosting</button>
+        </div>
 
       </section>
     </>
